bd69ad646495abec404718f5bbaa5ef7afd3a710
karlo <karlolerga16@gmail.com>
2026-09-20 19:33:52 +0200

    refactor: keep database connection in jdbc url

--- src/main/java/hr/unizd/autocare/app/DatabaseConfig.java
+++ src/main/java/hr/unizd/autocare/app/DatabaseConfig.java
@@ -7,29 +7,18 @@ import java.util.Map;
 
 /** Otvara EntityManagerFactory za Azure SQL bazu. */
 public final class DatabaseConfig {
-  private static final String HOST = "YOUR_AZURE_SQL_HOST";
-  private static final String PORT = "1433";
-  private static final String DATABASE = "YOUR_DATABASE_NAME";
-  private static final String USERNAME = "YOUR_SQL_USERNAME";
-  private static final String PASSWORD = "YOUR_SQL_PASSWORD";
-
   private DatabaseConfig() {}
 
   public static EntityManagerFactory open() {
     Map<String, Object> properties = new HashMap<>();
     String jdbcUrl =
-        "jdbc:sqlserver://"
-            + HOST
-            + ":"
-            + PORT
-            + ";databaseName="
-            + DATABASE
-            + ";encrypt=true;trustServerCertificate=false;";
+        "jdbc:sqlserver://YOUR_AZURE_SQL_HOST:1433;"
+            + "databaseName=YOUR_DATABASE_NAME;"
+            + "user=YOUR_SQL_USERNAME;"
+            + "password=YOUR_SQL_PASSWORD;"
+            + "encrypt=true;trustServerCertificate=false;";
 
     properties.put("jakarta.persistence.jdbc.url", jdbcUrl);
-    properties.put("jakarta.persistence.jdbc.user", USERNAME);
-    properties.put("jakarta.persistence.jdbc.password", PASSWORD);
-
     return Persistence.createEntityManagerFactory("autocare", properties);
   }
 }
